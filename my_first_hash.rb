0bdec6880889188e888204a3d8c8425a1d57c2ef
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-15 12:48:37 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -29,7 +29,7 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 shipping_manifest["muskets"] = 2
-
+puts shipping_manifest
 
   # add 4 gun powder to the shipping_manifest hash below
 shipping_manifest["gunpowder"] = 4
